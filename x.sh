ee78423e1b8bf8fb3ee4542f4ba3e6d7d5ab47c1
dishapatel010 <71930916+dishapatel010@users.noreply.github.com>
2021-11-01 13:43:37 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -3,7 +3,6 @@
 ## Bot Setup
 git clone https://github.com/xdwtf/MERGE-BOT bot
 cp -r bot/helpers bot/assets bot/bot.py bot/config.py ./
-pip3 install -r bot/requirements.txt -q --no-cache-dir
 
 #worker
 python3 bot.py
